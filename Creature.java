c6a8ebc92d7cfa8160e39913b1b5133c4690ae7f
veroniquejunli <145383005+veroniquejunli@users.noreply.github.com>
2023-11-13 12:03:22 -0700

    Create Creature.java

---
+++ Creature.java
@@ -0,0 +1,20 @@
+
+public class Creature implements Reproduction, Death {
+    private String name;
+    public Creature(String n){
+        name = n;
+        System.out.println("");
+        
+    }
+
+    @Override
+    public void reproduce(){
+        System.out.println(name + " now has a child!");
+
+    }
+
+    @Override
+    public void die(){
+        System.out.println(name + " has died.");
+    }
+}
