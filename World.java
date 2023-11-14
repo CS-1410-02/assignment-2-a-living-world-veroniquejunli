766a63312c763ccdedce5959596e4ad2f573f945
veroniquejunli <145383005+veroniquejunli@users.noreply.github.com>
2023-11-13 21:24:44 -0700

    Update World.java

--- World.java
+++ World.java
@@ -1,20 +1,24 @@
-package com.example.test;
+package com.example.project;
 
 import java.util.ArrayList;
 
 public class World {
-
+    
     ArrayList<Critter> Creatures = new ArrayList<>();
+
     public static void main(String[] args){
-        Creatures.add(new Critter());
-        while(true){
-            for(Critter c: Creatures){
-                if(c.die()){
-                    --Critter;
-                }
-                if(c.reproduce()){
-                    ++Critter;
-                }
+
+        public void createCreature(){
+            Creatures.add(new Critter());
+                while(true){
+                    for(Critter c: Creatures){
+                        if(c.die()){
+                            --Critter;
+                        }
+                      if(c.reproduce()){
+                            ++Critter;
+                        }
+          }
 
     }
 }
