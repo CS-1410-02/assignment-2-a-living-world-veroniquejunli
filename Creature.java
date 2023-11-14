2131d86577ec1a33841925ad2f54062b0e0bfe14
veroniquejunli <145383005+veroniquejunli@users.noreply.github.com>
2023-11-13 21:56:05 -0700

    Update Creature.java

--- Creature.java
+++ Creature.java
@@ -1,13 +1,22 @@
 package com.example.project;
 
-public interface Creature implements Reproduction, Death {
+public interface Creature implements Feed, Reproduction, Death {
     private String name;
     public Creature(String n){
         name = n;
         System.out.println("");
         
     }
+    @Override
+    public void feedMeSeymour(){
+        System.out.println("");
+    }
 
+
+    @Override
+    public void 
+
+    
     @Override
     public void reproduce(){
         System.out.println(name + " now has a child!");
